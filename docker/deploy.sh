045d3792b5f0efd4d4986d407b083f2353996f1f
Jyoungjo <zimbote0411@gmail.com>
2023-08-31 09:11:29 +0900

    bug: 쉘 스크립트 수정

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-IS_GREEN=$(docker ps | grep green)
+IS_GREEN=$(docker ps | grep web_green)
 
 if [ -z "$IS_GREEN" ]; then # blue라면
 
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull green # green으로 이미지를 내려받습니다.
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_green # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d green # green 컨테이너 실행
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d web_green # green 컨테이너 실행
   while [ 1 = 1 ]; do
     echo "3. green health check..."
     sleep 3
@@ -27,15 +27,15 @@ if [ -z "$IS_GREEN" ]; then # blue라면
   sudo nginx -s reload
 
   echo "5. blue container down"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml stop blue
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml stop web_blue
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull blue
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_blue
 
   echo "2. blue container up"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d blue
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d web_blue
 
   while [ 1 = 1 ]; do
     echo "3. blue health check..."
@@ -53,5 +53,5 @@ else
   sudo nginx -s reload
 
   echo "5. green container down"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml stop green
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml stop web_green
 fi

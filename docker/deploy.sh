2640251f64b91474a81137ab27b5ca1ce11ed30e
Jyoungjo <zimbote0411@gmail.com>
2023-08-31 16:11:23 +0900

    refactor: deploy.sh 수정

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -7,7 +7,7 @@ if [ -z "$IS_GREEN" ]; then # blue라면
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_green # green으로 이미지를 내려받습니다.
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_green # green 으로 이미지 pull
 
   echo "2. green container up"
   docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d web_green # green 컨테이너 실행
@@ -15,7 +15,7 @@ if [ -z "$IS_GREEN" ]; then # blue라면
     echo "3. green health check..."
     sleep 3
 
-    REQUEST=$(curl -I http://ec2-43-201-68-117.ap-northeast-2.compute.amazonaws.com:8082) # green으로 request
+    REQUEST=$(curl -I http://ec2-43-201-68-117.ap-northeast-2.compute.amazonaws.com:8082) # green 으로 request
     if [ -n "$REQUEST" ]; then               # 서비스 가능하면 health check 중지
       echo "health check success"
       break
@@ -32,15 +32,15 @@ else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_blue
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml pull web_blue # blue 로 이미지 pull
 
   echo "2. blue container up"
-  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d web_blue
+  docker-compose -f /home/ubuntu/docker/docker-compose-app.yml up -d web_blue # blue 컨테이너 실행
 
   while [ 1 = 1 ]; do
     echo "3. blue health check..."
     sleep 3
-    REQUEST=$(curl -I http://ec2-43-201-68-117.ap-northeast-2.compute.amazonaws.com:8081) # blue로 request
+    REQUEST=$(curl -I http://ec2-43-201-68-117.ap-northeast-2.compute.amazonaws.com:8081) # blue 로 request
     if [ -n "$REQUEST" ]; then               # 서비스 가능하면 health check 중지
       echo "health check success"
       break

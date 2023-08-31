b0bea61361239018bb7caa257f3dd3eedaafc9ea
Jyoungjo <zimbote0411@gmail.com>
2023-08-31 15:37:48 +0900

    refactor: config 수정

--- src/main/java/com/example/hiddenpiece/config/WebSecurityConfig.java
+++ src/main/java/com/example/hiddenpiece/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -72,25 +72,25 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        // 모든 접근 가능
-        configuration.setAllowedOrigins(List.of("*"));
-        // HTTP method 지정
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        // Access-Control-Allow-Credentials 헤더 설정
-        configuration.setAllowCredentials(true);
-        // 클라이언트가 전송할 수 있는 헤더 값을 설정
-        configuration.addAllowedHeader("*");
-        // 클라이언트가 다시 preflight 요청을 보내지 않아도 되는 시간을 설정
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        // 모든 경로 CORS 적용
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        // 모든 접근 가능
+//        configuration.setAllowedOrigins(List.of("*"));
+//        // HTTP method 지정
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+//        // Access-Control-Allow-Credentials 헤더 설정
+//        configuration.setAllowCredentials(true);
+//        // 클라이언트가 전송할 수 있는 헤더 값을 설정
+//        configuration.addAllowedHeader("*");
+//        // 클라이언트가 다시 preflight 요청을 보내지 않아도 되는 시간을 설정
+//        configuration.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        // 모든 경로 CORS 적용
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
     public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
         @Override

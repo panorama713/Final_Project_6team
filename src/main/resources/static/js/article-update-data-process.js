79dc13074ede56de28974b8c566c662debd019ab
sssssn <doitforyourself0603@gmail.com>
2023-09-06 01:50:09 +0900

    refactor: 사용자 정의 예외 처리 적용

--- src/main/resources/static/js/article-update-data-process.js
+++ src/main/resources/static/js/article-update-data-process.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetchArticleInfo();
 
     var submitButton = document.getElementById("article-update");
@@ -31,7 +31,7 @@ function updateArticle() {
         type: type
     };
 
-    formData.append("params", new Blob([JSON.stringify(jsonParams)], { type: "application/json" }));
+    formData.append("params", new Blob([JSON.stringify(jsonParams)], {type: "application/json"}));
 
     for (var i = 0; i < images.length; i++) {
         formData.append("images", images[i]);
@@ -43,9 +43,21 @@ function updateArticle() {
         method: "PUT",
         body: formData
     })
-        .then(data => {
-            alert("게시글이 수정되었습니다.");
-            window.location.href = "/views/articles/"+articleId;
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    throw new Error(error.message || "게시글 수정 중 오류가 발생했습니다.");
+                });
+            }
+            if (response.status === 204 || response.status === 205) {
+                alert("게시글이 수정되었습니다.");
+                window.location.href = "/views/articles/" + articleId;
+                return;
+            }
+            return response.json();
         })
-        .catch(error => console.error("게시글 수정 오류:", error));
+        .catch(error => {
+            console.error("게시글 수정 오류:", error);
+            alert(error.message);
+        });
 }

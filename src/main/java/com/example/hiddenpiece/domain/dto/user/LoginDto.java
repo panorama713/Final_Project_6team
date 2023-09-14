41aecbfc2c9212694feba6bf24f51ecd5bb29033
sssssn <doitforyourself0603@gmail.com>
2023-09-14 18:30:18 +0900

    feat: 로그인 시 해당 필드 유효성 검사 적용

--- src/main/java/com/example/hiddenpiece/domain/dto/user/LoginDto.java
+++ src/main/java/com/example/hiddenpiece/domain/dto/user/LoginDto.java
@@ -1,5 +1,7 @@
 package com.example.hiddenpiece.domain.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +10,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginDto {
+    @NotBlank(message = "아이디는 필수입니다.")
+    @Pattern(regexp = "^[a-z][a-z0-9]{4,14}$", message = "아이디는 5~15자의 영문 소문자, 숫자만 사용 가능합니다.\"")
     private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[`~₩!@#$%^&*]).{7,19}$", message = "비밀번호는 8~20자의 영문, 숫자, 특수문자를 모두 포함해야 합니다.")
     private String password;
 
     @Builder

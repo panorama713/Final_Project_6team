4da2e4ec867431af72bf28365ce17eb210a41e5a
sssssn <doitforyourself0603@gmail.com>
2023-09-14 19:00:20 +0900

    feat: 정보 수정 시 해당 필드 유효성 검사 적용

---
+++ src/main/resources/static/js/update-info-ui.js
@@ -0,0 +1,33 @@
+// 동적 UI 구성
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    createInputFields();
+}
+
+// 사용자 정보 입력 필드를 동적으로 생성
+function createInputFields() {
+    const container = document.querySelector('.user-info-inputs');
+
+    container.querySelectorAll('.input-row').forEach(row => {
+        const label = row.getAttribute('data-label');
+        const type = row.getAttribute('data-type');
+        const id = row.getAttribute('data-id');
+
+        let errorElement = '';
+        if (id === 'newPassword' || id === 'passwordCheck' || id === 'newEmail' || id === 'newPhone') {
+            errorElement = `<div id="${id}Error" class="text-danger"></div>`;
+        }
+
+        // HTML 구조를 동적으로 생성
+        row.innerHTML = `
+            <div class="row mb-3">
+                <label for="${id}" class="form-label col-sm-3">${label}</label>
+                <div class="col-sm-9">
+                    <input type="${type}" class="form-control" id="${id}" name="${id}" required>
+                    ${errorElement}
+                </div>
+            </div>
+        `;
+    });
+}

70111d9b713251c843fdbf2ea015f750dcf71843
Jyoungjo <zimbote0411@gmail.com>
2023-09-15 10:41:40 +0900

    chore: 사용하지 않는 import 문 제거

--- src/main/java/com/example/hiddenpiece/HiddenPieceApplication.java
+++ src/main/java/com/example/hiddenpiece/HiddenPieceApplication.java
@@ -1,8 +1,6 @@
 package com.example.hiddenpiece;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
